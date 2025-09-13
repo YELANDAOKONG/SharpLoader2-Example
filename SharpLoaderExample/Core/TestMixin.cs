d0f78915edafe7030ed42737f45efc00e559cdfb
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-14 03:28:30 +0800

    优化代码

--- SharpLoaderExample/Core/TestMixin.cs
+++ SharpLoaderExample/Core/TestMixin.cs
@@ -1,5 +1,6 @@
 using SharpASM.Models;
 using SharpASM.Models.Code;
+using SharpASM.Models.Struct.Attribute;
 using SharpMixin.Attributes;
 using SharpMixin.Models;
 
@@ -11,8 +12,9 @@ public class TestMixin
         "method_5749", 
         "(Lnet/minecraft/class_11368;)V", 
         NameType.Default)]
-    public static List<Code> TestMixinMethod(Class clazz, List<Code> codes)
+    public static CodeAttributeStruct TestMixinMethod(Class clazz, CodeAttributeStruct attribute)
     {
+        List<Code> codes = attribute.GetCode();
         var helper = clazz.GetConstantPoolHelper();
         var fieldName = "field_7520";
         var newIndex = helper.NewInteger(114514);
@@ -24,7 +26,8 @@ public class TestMixin
         codes.Add(new Code(OperationCode.PUTFIELD, new[] { Operand.FieldRef(fieldRefIndex) }));
         codes.Add(new Code(OperationCode.RETURN));
 
-        clazz.ConstantPool = helper.ToList();        
-        return codes;
+        clazz.ConstantPool = helper.ToList();     
+        attribute.SetCode(codes);
+        return attribute;
     }
 }
